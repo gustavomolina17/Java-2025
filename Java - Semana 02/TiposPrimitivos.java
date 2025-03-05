9dab9176a881456254ddf640ced5c7c15153dbc9
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2025-03-05 19:39:24 -0300

    Create TiposPrimitivos.java

---
+++ Java - Semana 02/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+        byte valorByte = 17;
+        short valorShort = 200;
+        int valorInt = 58;
+        long valorLong = 10000000000L;
+        float valorFloat = 10.5f;
+        double valorDouble = 24.99;
+        boolean valorBoolean = true;
+        char valorChar = 'A';
+
+
+        System.out.println("Valor do byte: " + valorByte);
+        System.out.println("Valor do short: " + valorShort);
+        System.out.println("Valor do int: " + valorInt);
+        System.out.println("Valor do long: " + valorLong);
+        System.out.println("Valor do float: " + valorFloat);
+        System.out.println("Valor do double: " + valorDouble);
+        System.out.println("Valor do boolean: " + valorBoolean);
+        System.out.println("Valor do char: " + valorChar);
+    }
+}
