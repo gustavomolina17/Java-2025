d652e662180f50335e5482b253f6ff38d12c9b4a
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2025-04-22 16:57:06 -0300

    Create Diretor.java

---
+++ Java - Lista de Exercicios01/Diretor.java
@@ -0,0 +1,15 @@
+class Diretor extends Funcionario {
+    private double participacaoLucros;
+
+
+    public Diretor(double salarioFixo, double participacaoLucros) {
+        super(salarioFixo);
+        this.participacaoLucros = participacaoLucros;
+    }
+
+
+    @Override
+    public double calcularPagamento() {
+        return salarioFixo + participacaoLucros;
+    }
+}
