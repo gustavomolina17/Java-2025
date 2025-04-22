788b38744bd9535c89940e97051f06f49b8eb2af
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2025-04-22 16:57:25 -0300

    Create Ex1_Retangulo.java

---
+++ Java - Lista de Exercicios01/Ex1_Retangulo.java
@@ -0,0 +1,19 @@
+class Ex1_Retangulo extends Ex1_Forma {
+    private double largura;
+    private double altura;
+
+    public Ex1_Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return largura * altura;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * (largura + altura);
+    }
+}
